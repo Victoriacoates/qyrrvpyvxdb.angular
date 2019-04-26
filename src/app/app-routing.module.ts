4dfd2c6c498fdb6fb68a467ca480b19868c689df
Victoriacoates <victoriacoates0@gmail.com>
2019-04-26 05:34:22 -0500

    5 files modified, 7 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,17 @@ import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
+import { AnalysisComponent } from './start-analysis/analysis.component';
+
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'rules', component: HeroesComponent },
+
+
+
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,18 @@ import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppRoutingModule }     from './app-routing.module';
 
+import { HeaderComponent } from './extras/header.component';
+import { FooterComponent } from './extras/footer.component';
+
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroSearchComponent }  from './hero-search/hero-search.component';
 import { MessagesComponent }    from './messages/messages.component';
+
+
+
 
 @NgModule({
   imports: [
@@ -30,6 +36,11 @@ import { MessagesComponent }    from './messages/messages.component';
     )
   ],
   declarations: [
+    HeaderComponent,
+    FooterComponent,
+
+  
+
     AppComponent,
     DashboardComponent,
     HeroesComponent,
